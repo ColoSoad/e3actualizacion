f256af6b7f6a31a7c585116979ae29679e1b9d7e
Joel <joelhernanmiranda@gmail.com>
2025-01-22 12:44:12 -0300

    agregando footer nuevo, con funcionalidad js local y tambien se procede a inicar proyecto multi-idiomas

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const facebook = document.querySelector('#icon-facebook');
+const instagram = document.querySelector('#icon-instagram');
+const linkedin = document.querySelector('#icon-linkedin');
+
+facebook.addEventListener('click', (e) => {
+    window.open('https://www.facebook.com/profile.php?id=100071002556214', '_blank');
+});
+
+instagram.addEventListener('click', (e) => {
+    window.open('https://www.instagram.com/estudio.3_arq?igsh=MXhqMTZpb2Vtb3Focw==', '_blank');
+});
